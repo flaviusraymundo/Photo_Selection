b7c29427c5e28def249eeee1270ff5445a765d89
flaviusraymundo <141464857+flaviusraymundo@users.noreply.github.com>
2025-08-07 10:21:52 -0300

    Configure for hosting platforms

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NOME_DO_SEU_REPOSITORIO/',
-  base: '/NOME_DO_SEU_REPOSITORIO/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
