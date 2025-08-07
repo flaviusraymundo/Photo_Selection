adf95a6579b7d1ee25a1584f3914441b88a5a4f1
flaviusraymundo <141464857+flaviusraymundo@users.noreply.github.com>
2025-08-07 10:13:38 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/NOME_DO_SEU_REPOSITORIO/',
+  base: '/NOME_DO_SEU_REPOSITORIO/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
