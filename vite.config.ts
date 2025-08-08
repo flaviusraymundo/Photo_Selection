c1f5744955a58d8ad9a81f5cdc5ba597be0ae0dc
flaviusraymundo <141464857+flaviusraymundo@users.noreply.github.com>
2025-08-08 18:39:43 -0300

    Fix paths for GitHub Pages with repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Photo_Selection/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
